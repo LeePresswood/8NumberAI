df36e8ed696380cef7a75c8c839e03f1ca8e4b71
Lee Presswood <leepresswood@carfax.com>
2015-12-17 11:18:52 -0600

    Added board class

---
+++ src/com/leepresswood/numberai/Board.java
@@ -0,0 +1,21 @@
+package com.leepresswood.numberai;
+
+/**
+ * Created by leepresswood on 12/17/15.
+ */
+public class Board{
+	public int[][] numbers;
+
+	@Override
+	public String toString(){
+		String string = "";
+
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+
+			}
+		}
+
+		return string;
+	}
+}
--- src/com/leepresswood/numberai/Main.java
+++ src/com/leepresswood/numberai/Main.java
@@ -3,6 +3,6 @@ package com.leepresswood.numberai;
 public class Main {
 
     public static void main(String[] args) {
-	
+
     }
 }
