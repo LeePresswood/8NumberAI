6b082c2850cdde1703a461c76ad85d1d961ca350
Lee Presswood <leepresswood@carfax.com>
2015-12-17 11:39:07 -0600

    Added board init

--- src/com/leepresswood/numberai/Board.java
+++ src/com/leepresswood/numberai/Board.java
@@ -1,21 +1,91 @@
 package com.leepresswood.numberai;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Created by leepresswood on 12/17/15.
  */
 public class Board{
-	public int[][] numbers;
+	public byte[][] numbers;
+
+	public Board(){
+		initializeBoard();
+	}
+
+	public Board(Board to_copy){
+		this.numbers = Arrays.copyOf(to_copy.numbers.clone(), to_copy.numbers.length);
+	}
+
+	private void initializeBoard(){
+		numbers = new byte[3][3];
+		Random random = new Random();
+
+		//Init all with -1.
+		for(byte j = 0; j < 3; j++){
+			for(byte i = 0; i < 3; i++){
+				numbers[j][i] = -1;
+			}
+		}
+
+		//Set all numbers.
+		byte counter = 0;
+		while(counter != 10){
+			//Choose a position at random.
+			byte x = (byte) random.nextInt(3);
+			byte y = (byte) random.nextInt(3);
+
+			//If this is free, place and move on.
+			if(numbers[y][x] == -1){
+				numbers[y][x] = counter++;
+			}
+		}
+	}
+
+	public Position getPositionOf(byte position){
+		for(byte j = 0; j < 3; j++){
+			for(byte i = 0; i < 3; i++){
+				if(numbers[j][i] == position){
+					return new Position(i, j);
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public void canShift(Direction direction){
+
+	}
 
 	@Override
 	public String toString(){
 		String string = "";
 
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++){
-
+		for(byte j = 0; j < 3; j++){
+			for(byte i = 0; i < 3; i++){
+				if(numbers[j][i] == 0){
+					string += " _ ";
+				}
+				else{
+					string += " " + numbers[j][i] + " ";
+				}
 			}
+			string += "\n";
 		}
 
 		return string;
+	}
+
+	public class Position{
+		public byte x, y;
+		public Position(byte x, byte y){
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public enum Direction{
+		UP, DOWN, LEFT, RIGHT
 	}
 }
