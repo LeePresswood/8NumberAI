ab89bf3581f44afacbdb4dadcf444fa3db6f4ce4
Lee Presswood <leepresswood@carfax.com>
2015-12-17 16:13:20 -0600

    Added solver class.

--- src/com/leepresswood/numberai/Main.java
+++ src/com/leepresswood/numberai/Main.java
@@ -3,6 +3,6 @@ package com.leepresswood.numberai;
 public class Main {
 
     public static void main(String[] args) {
-
+        int moves = Solver.solve(new Board());
     }
 }
--- src/com/leepresswood/numberai/Solver.java
+++ src/com/leepresswood/numberai/Solver.java
@@ -4,4 +4,16 @@ package com.leepresswood.numberai;
  * Created by leepresswood on 12/17/15.
  */
 public class Solver{
+	/**
+	 * Solves the passed board.
+	 * @param board The board to work on.
+	 * @return Number of moves.
+	 */
+	public static int solve(Board board){
+		int moves = 0;
+
+
+
+		return moves;
+	}
 }
