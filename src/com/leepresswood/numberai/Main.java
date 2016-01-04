7efa43f48f35328f36db290956931599582e316c
Lee Presswood <leepresswood@carfax.com>
2016-01-04 14:04:19 -0600

    Moved to a recursive model.

--- src/com/leepresswood/numberai/Main.java
+++ src/com/leepresswood/numberai/Main.java
@@ -3,7 +3,13 @@ package com.leepresswood.numberai;
 public class Main {
 
     public static void main(String[] args) {
-        int moves = Solver.solve(new Board());
-        System.out.print(moves);
+        int moves = Solver.solve(new Board(), 0);
+
+        if(moves != -1){
+            System.out.println(moves);
+        }
+        else{
+            System.out.println("Failure.");
+        }
     }
 }
--- src/com/leepresswood/numberai/Solver.java
+++ src/com/leepresswood/numberai/Solver.java
@@ -9,28 +9,37 @@ public class Solver{
 	 * @param board The board to work on.
 	 * @return Number of moves.
 	 */
-	public static int solve(Board board){
-		int moves = 0;
-
+	public static int solve(Board board, int moves_previous){
+		if(moves_previous > 100){
+			return -1;
+		}
+		System.out.println(moves_previous);
 		//Get the current score of the board.
 		int current_score = board.getHeuristicScore();
-		while(current_score != 0){
-			//Every neighbor should be checked.
-			int neighbor = getBestNeighbor(board);
-
-			//Use this above neighbor to swap on the main board.
-			board.shift(board.getPositionOf(neighbor));
-
-			//Get ready for the next move.
-			moves++;
-			current_score = board.getHeuristicScore();
+		if(current_score == 0){
+			return moves_previous;
 		}
 
-		return moves;
+		//Recursively check best moves.
+		int[] neighbors = board.getNeighborsOfBlank();
+		int[] this_boards_moves = new int[neighbors.length];
+		Board[] neighbor_boards = new Board[neighbors.length];
+		int best_moves = 9999;
+		for(int i = 0; i < neighbors.length; i++){
+			neighbor_boards[i] = new Board(board);
+			neighbor_boards[i].shift(neighbor_boards[i].getPositionOf(neighbors[i]));
+			this_boards_moves[i] = solve(neighbor_boards[i], moves_previous + 1);
+
+			if(this_boards_moves[i] != -1 && this_boards_moves[i] < best_moves){
+				best_moves = this_boards_moves[i];
+			}
+		}
+
+		return (best_moves == 9999 ? -1 : best_moves);
 	}
 
 	private static int getBestNeighbor(Board board){
-		Integer[] neighbors = board.getNeighborsOfBlank();
+		int[] neighbors = board.getNeighborsOfBlank();
 		int best = -1;
 
 		//Make clones of the board for each neighbor.
